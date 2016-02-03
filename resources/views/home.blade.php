ca6a15d0476d46093eb11cfc68a8329d84b35898
Vitali <vitali.yakush@gmail.com>
2016-02-03 22:01:12 +0300

    test auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    Hello, {{ \Illuminate\Support\Facades\Auth::user()->name  }} !!!
                 </div>
             </div>
         </div>
